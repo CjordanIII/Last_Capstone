d98dd2982aff681ab109d2362289c121e8b7d87e
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-25 12:12:08 -0500

    add redirect

--- scripts/services/user.js
+++ scripts/services/user.js
@@ -5,12 +5,16 @@ const createUser = async (body) => {
       {
         method: "POST",
         headers: {
-          "content-Type": "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(body),
       }
     );
     console.log(result);
+
+    if (result.ok) {
+      window.location.href = "http://127.0.0.1:5500/";
+    }
   } catch (w) {
     console.log(w);
   }
